2bc2107a4708c14affc16298693a419b5df6c803
Shahad Mahmud <shahad9381@gmail.com>
2020-08-03 17:31:56 +0600

    initial update

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Shahad Mahmud</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@700&family=Gentium+Basic&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <header>
+        <nav id="navbar" class="navbar">
+            <ul class="nav-list">
+                <li>
+                    <a href="#welcome-section">Home</a>
+                </li>
+                <li>
+                    <a href="#about_me">About</a>
+                </li>
+                <li>
+                    <a href="#projects">Projects</a>
+                </li>
+                <li>
+                    <a href="#skill">Skills</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <section id="welcome-section" class="welcome">
+        <div class="wel_img">
+            <center>
+                <img class="profile_image" alt="profile image" src="assets/images/profile.png"/>
+            </center>
+        </div>
+        <div class="wel_intro">
+            I'm<br>
+            <h1>Shahad Mahmud</h1>
+            who is currently a student, but a passonate larner and challenge lover.
+        </div>
+    </section>
+
+    <section id="about_me" class="about_me">
+        <h1>About me</h1>
+        <p>
+        I'm a final semester student at Rajshahi University of Engineering & Technology. I'm completing my <b>Bachelor of Science in Engineering</b> degree from the Department of Computer Science and Engineering. I'm passionate and motivated to work and develop new skills in Software Engineering and Machine Learning domains. 
+        </p>
+
+        <p>
+        During my time in university, I have participated in sports programming, developed several Android and web-based applications and conducted my thesis work using different Deep Learning techniques. I have solved several problems and took part in programming contests in Codeforces, Hackerrank, SPOJ, UVA and on other platforms which helped me to sharpen my analytical skills. Besides academic syllabus, I have joined several training programs and completed courses to develop and concrete different skills. I love to keep learning and experiment with what I am learning. I try to help others to spread my knowledge and try to learn from others also.
+        </p>
+
+        <p>
+        I have been participating in different co-curricular activities and social servicing since my college life. In different times I acted on different leading roles which helped me to develop several soft skills like leadership, management etc.
+        </p>
+
+        <p>
+        I love to work in a co-operative workspace where I can make the best use of my skills to solve challenging and real-life problems with continuous learning opportunities.
+        </p>
+
+        <p style="text-align: center; margin-top:8vh;">Find me on:</p>
+        <div class="social_list">
+            <ul>
+                <li>
+                    <a href="https://www.linkedin.com/in/shahad-mahmud/" target="_blank">
+                        <img class="social_icon" src="assets/images/icons/linkedin.svg" alt="Linkedin"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/shahad-mahmud" target="_blank">
+                        <img class="social_icon" src="assets/images/icons/github.svg" alt="Github"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/shahad_chowdhury/" target="_blank">
+                        <img class="social_icon" src="assets/images/icons/insta.svg" alt="Instagram"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/shahad001/" target="_blank">
+                        <img class="social_icon" src="assets/images/icons/facebook.svg" alt="Facebook"/>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section id="projects" class="project_section">
+        <h1>Projects</h1>
+        <div class="project-card-holder">
+            <div class="project">
+                <div class="project-tile">
+                Portfolio Portfolio Portfolio 
+                Portfolio Portfolio Portfolio 
+                </div>
+                <div class="project-details">
+                This is project details. This is project details. This is project details. This is project details. This is project details. This is project details. This is project details. This is project details. This is project details. This is project details. This is project details. This is project details. This is project details. This is project details. This is project details. 
+                </div>
+                <div class="project_details_btn">
+                    View project
+                </div>
+            </div>
+            <div class="project">
+                <div class="project-tile">
+                Blog
+                </div>
+            </div>
+            <div class="project">
+                <div class="project-tile">
+                NULL
+                </div>
+            </div>
+            <div class="project">
+                <div class="project-tile">
+                NULL 2
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="skills" class="skill_section">
+        <h1>Skills</h1>
+        <div class="skill_container">
+            <div class="bar learning" data-skill="TDD"></div>
+            <div class="bar back basic" data-skill="Python"></div>
+            <div class="bar back intermediate" data-skill="C#"></div>
+            <div class="bar front advanced" data-skill="CSS3"></div>
+            <div class="bar front expert" data-skill="HTML5"></div>
+        </div>
+    </section>
+    <section id="experience" class="experience_section">
+        <h1>Experiences</h1>
+        
+    </section>
+
+    <section id="education" class="edu_section">
+        <h1>Education</h1>
+        
+    </section>
+
+    <section id="certification" class="certifications_section">
+        <h1>Certifications</h1>
+        
+    </section>
+
+    <section id="footer" class="footer_section">
+        <h1>footer</h1>
+        Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+        Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+    </section>
+</body>
+</html>
